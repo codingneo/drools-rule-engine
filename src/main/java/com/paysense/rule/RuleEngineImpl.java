9fbe4758ce213cf08320d571ba70ba3df6a44def
Chen Yanhui <chenyanhui@chens-air.gateway.2wire.net>
2015-08-02 22:32:50 +0800

    changes for dynamically load rules

--- src/main/java/com/paysense/rule/RuleEngineImpl.java
+++ src/main/java/com/paysense/rule/RuleEngineImpl.java
@@ -1,8 +1,15 @@
 package com.paysense.rule;
 
+import java.io.InputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.drools.core.io.impl.UrlResource;
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieScanner;
-import org.kie.api.builder.ReleaseId;
+import org.kie.api.builder.KieModule;
+import org.kie.api.builder.KieRepository;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,39 +20,71 @@ import com.paysense.entity.TranObjectContainer;
 @Component
 public class RuleEngineImpl implements RuleEngine {
 
-	private static KieContainer kContainer;
-	
-	@Value("${rules.groupId}")
-	private String rulesGroupId;
+	private final Logger logger = Logger.getLogger(this.getClass());
 
-	@Value("${rules.artifactId}")
-	private String rulesArtifactId;
+	@Value("${rules.url}")
+	private String rulesUrl;
 
-	@Value("${rules.version}")
-	private String rulesVersion;	
+	@Value("${rules.user}")
+	private String rulesUser;
+
+	@Value("${rules.password}")
+	private String rulesPassword;
 
 	@Value("${kiescanner.interval}")
-	private int kiescannerInterval;	
-	
+	private int kiescannerInterval;
+
+	private KieContainer kContainer;
+
+	private ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(3);
+
+	public RuleEngineImpl() {
+		logger.info("Inside RuleEngineImpl constructor");
+		this.scheduledThreadPool.scheduleAtFixedRate(new InitKieContainerWorker(), 10, 10, TimeUnit.SECONDS);
+	}
+
 	@Override
 	public int process(TranObjectContainer container) {
 
-		if(kContainer==null) {
-			
-			KieServices kieServices = KieServices.Factory.get();
-			ReleaseId releaseId = kieServices.newReleaseId(rulesGroupId, rulesArtifactId, rulesVersion);
-			kContainer = kieServices.newKieContainer( releaseId );
-			
-			KieScanner kScanner = kieServices.newKieScanner( kContainer );
-			// Start the KieScanner polling the Maven repository every 10 seconds
-			kScanner.start( kiescannerInterval * 1000 );
-		}
-		
+		kContainer = initKieContainer();
+
 		KieSession session = kContainer.newKieSession();
 		session.insert(container);
-		
+
 		session.fireAllRules();
-		
+
+		session.dispose();
+
 		return container.getNewTransaction().getStatus();
+	}
+
+	private synchronized KieContainer initKieContainer() {
+
+		logger.info("Inside RuleEngineImpl initKieContainer");
+
+		try {
+
+			KieServices ks = KieServices.Factory.get();
+			KieRepository kr = ks.getRepository();
+			UrlResource urlResource = (UrlResource) ks.getResources().newUrlResource(rulesUrl);
+			urlResource.setUsername(rulesUser);
+			urlResource.setPassword(rulesPassword);
+			urlResource.setBasicAuthentication("enabled");
+			InputStream is = urlResource.getInputStream();
+			KieModule kModule = kr.addKieModule(ks.getResources().newInputStreamResource(is));
+
+			return ks.newKieContainer(kModule.getReleaseId());
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private class InitKieContainerWorker implements Runnable {
+
+		@Override
+		public void run() {
+			kContainer = initKieContainer();
+		}
 	}
 }
